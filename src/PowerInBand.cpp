f7439e7ce2d47f3065cdcd5d16621a717d515486
Todd Rylaarsdam <todd@toddr.org>
2023-06-26 11:51:00 -0500

    FFT

--- src/PowerInBand.cpp
+++ src/PowerInBand.cpp
@@ -1,1 +1,56 @@
 #include "PowerInBand.h"
+
+namespace PowerInBand 
+{
+	double Compute(double *samples, unsigned long len, double sampling_freq, double lower_freq, double upper_freq)
+	{
+		return Compute(samples, len, sampling_freq, lower_freq, upper_freq, FFT_WIN_TYP_WELCH);
+	}
+
+	double Compute(double *samples, unsigned long len, double sampling_freq, double lower_freq, double upper_freq, FFTWindow window_method)
+	{
+		return Compute(samples, len, sampling_freq, lower_freq, upper_freq, window_method, false);
+	}
+
+	double Compute(double *samples, unsigned long len, double sampling_freq, double lower_freq, double upper_freq, FFTWindow window_method, bool use_SDRAM)
+	{
+		double* vReal;
+		double* vImag;
+		// SDRAM only compatible with Portenta H7's M7 core
+		#ifdef CORE_CM7
+		if(use_SDRAM)
+		{
+			SDRAM.begin();
+			vReal = (double*)SDRAM.malloc(len * sizeof(double));
+			vImag = (double*)SDRAM.malloc(len * sizeof(double));
+		}
+		else
+		{
+			vReal = (double*)malloc(len * sizeof(double));
+			vImag = (double*)malloc(len * sizeof(double));
+		}
+		#else
+		// Force use of SRAM on non-M7 core
+		vReal = (double*)malloc(len * sizeof(double));
+		vImag = (double*)malloc(len * sizeof(double));
+		#endif
+
+		// initialize FFT
+		arduinoFFT FFT = arduinoFFT(vReal, vImag, len, sampling_freq);
+
+		// copy the samples into the real part of the FFT input
+		for (unsigned long i = 0; i < len; i++)
+		{
+			vReal[i] = samples[i];
+		}
+
+		// Window the data using Welch's method
+		FFT.Windowing(window_method, FFT_FORWARD);
+		// Compute FFT
+		FFT.Compute(FFT_FORWARD);
+		// Convert to magnitude
+		FFT.ComplexToMagnitude();
+
+		return 0.0;
+	}
+}
--- src/PowerInBand.h
+++ src/PowerInBand.h
@@ -1,2 +1,38 @@
 #pragma once
+#include "../arduinoFFT/src/arduinoFFT.h"
+#ifdef CORE_CM7 
+#include "SDRAM.h"
+#endif
 
+namespace PowerInBand
+{
+	/// @brief Computers the power in band of the given sample buffer using the default of Welch's method for windowing, and SRAM
+	/// @param samples Pointer to the raw data
+	/// @param len Amount of data to be computed - this *must* be a power of 2
+	/// @param sampling_freq Sampling frequency of the data
+	/// @param lower_freq Lower frequency of the band
+	/// @param upper_freq Upper frequency of the band
+	/// @return The power in band of the specified frequency range
+	double Compute(double* samples, unsigned long len, double sampling_freq, double lower_freq, double upper_freq);
+
+	/// @brief Computers the power in band of the given sample buffer in SRAM
+	/// @param samples Pointer to the raw data
+	/// @param len Amount of data to be computed - this *must* be a power of 2
+	/// @param sampling_freq Sampling frequency of the data
+	/// @param lower_freq Lower frequency of the band
+	/// @param upper_freq Upper frequency of the band
+	/// @param window_method Windowing method to use
+	/// @return The power in band of the specified frequency range
+	double Compute(double *samples, unsigned long len, double sampling_freq, double lower_freq, double upper_freq, FFTWindow window_method);
+
+	/// @brief Computers the power in band of the given sample buffer with the option to use SDRAM
+	/// @param samples Pointer to the raw data
+	/// @param len Amount of data to be computed - this *must* be a power of 2
+	/// @param sampling_freq Sampling frequency of the data
+	/// @param lower_freq Lower frequency of the band
+	/// @param upper_freq Upper frequency of the band
+	/// @param window_method Windowing method to use
+	/// @param use_SDRAM Whether or not to use SDRAM for the FFT
+	/// @return The power in band of the specified frequency range
+	double Compute(double *samples, unsigned long len, double sampling_freq, double lower_freq, double upper_freq, FFTWindow window_method, bool use_SDRAM);
+}
